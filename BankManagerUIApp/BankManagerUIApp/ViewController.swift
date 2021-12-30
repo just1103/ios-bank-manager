5b3d92b746daca7719fb50851d093d67acf00dbe
just1103 <hyojuson1103@gmail.com>
2021-12-30 18:47:16 +0900

    chore: 빌드 문제 해결 #8

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -1,17 +1,34 @@
-//
-//  BankManagerUIApp - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let horizontalStackView: UIStackView = UIStackView()
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(horizontalStackView)
+        
+        let addCustomerButton = UIButton()
+        addCustomerButton.setTitle("고객 10명 추가", for: .normal)
+        addCustomerButton.setTitleColor(.blue, for: .normal)
+        
+        let resetButton = UIButton()
+        resetButton.setTitle("초기화", for: .normal)
+        resetButton.setTitleColor(.red, for: .normal)
+        
+        horizontalStackView.addArrangedSubview(addCustomerButton)
+        horizontalStackView.addArrangedSubview(resetButton)
+        
+        horizontalStackView.distribution = .fillEqually
+        
+        horizontalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        horizontalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+//        horizontalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        horizontalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
     }
+    
+    
 
 
 }
